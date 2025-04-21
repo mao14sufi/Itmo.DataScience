de5bb2e8de8429d4eac36bc2eccb54e0d77b0915
mao14sufi <167078726+mao14sufi@users.noreply.github.com>
2025-04-21 12:11:23 +0300

    Add files via upload

---
+++ SpamDetector.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpamDetector;
+Console.WriteLine("Write a message");
+
+
+//Load sample data
+var sampleData = new MLModel1.ModelInput()
+{
+	Subject =Console.ReadLine(),
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+
+Console.WriteLine($"Content - {result.Subject} - THIS IS  {(Convert.ToBoolean(result.IsSpam) ? "SPAM" : "NO SPAM")}. ");
